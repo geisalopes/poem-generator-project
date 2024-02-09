a05f42a2dc3dfd6c9e7b25284d194b803b854906
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-02-09 10:55:01 +0100

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -22,10 +22,6 @@ function generatePoem(event) {
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
-  console.log("Generate poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Conext: ${context}`);
-
   axios.get(apiUrl).then(displayPoem);
 }
 
